34ced71b09cbe4769c6ed58e68ae46e9f73384c6
nguyen <hiep.nguyenvan1@ncc.asia>
2023-08-10 00:51:58 +0700

    fix generate

--- tools/template/module/__moduleName__.module.ts
+++ tools/template/module/__moduleName__.module.ts
@@ -3,10 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { __moduleName__CamelCase__Entity } from './dto/__moduleName__.entity';
 import { __moduleName__CamelCase__Controller } from './__moduleName__.controller';
 import { __moduleName__CamelCase__Service } from './__moduleName__.service';
-import { RoleModule } from '@modules';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([__moduleName__CamelCase__Entity]), RoleModule],
+	imports: [TypeOrmModule.forFeature([__moduleName__CamelCase__Entity])],
 	controllers: [__moduleName__CamelCase__Controller],
 	providers: [__moduleName__CamelCase__Service],
 	exports: [__moduleName__CamelCase__Service],
--- tools/template/module/dto/__moduleName__.entity.ts
+++ tools/template/module/dto/__moduleName__.entity.ts
@@ -1,9 +1,8 @@
 import { Entity } from 'typeorm';
-import { AuditEntity } from '@entities';
+import { AuditEntity } from '@shared';
 
 @Entity({ name: '__moduleName__' })
 export class __moduleName__CamelCase__Entity extends AuditEntity {
-
 	constructor(partial: Partial<__moduleName__CamelCase__Entity>) {
 		super();
 		Object.assign(this, partial);
